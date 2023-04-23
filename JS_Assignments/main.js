23ca029f111b73c592b305cd6cae2b3a084c6b38
Rasmus M. Andersson <84081830+ralle121212@users.noreply.github.com>
2023-04-23 19:02:15 +0200

    Add files via upload

---
+++ JS_Assignments/main.js
@@ -0,0 +1,18 @@
+
+//change the value of the element with querySelector 
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 11;
+
+//how to create new element
+const tag = document.createElement("p");
+const text = document.createTextNode("This is the best way to learn");
+
+let array = document.arrays[
+    document.querySelector('h1'),
+    document.querySelector('h2'),
+    document.querySelector('h3'),
+]
+
+
+
+
